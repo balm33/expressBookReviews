5f7b99fc4f909038a279ca701af72aefc8f52773
Brendan <brendanalm3@gmail.com>
2025-06-24 13:57:51 -0400

    Task 2&3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,13 +21,25 @@ public_users.get('/', function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    // return res.status(300).json({ message: "Yet to be implemented" });
+    const ISBN = req.params.isbn;
+    res.send(books[ISBN])
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    // return res.status(300).json({ message: "Yet to be implemented" });
+    const author = req.params.author;
+    let booksByAuthor = [];
+
+    for (let isbn in books) {
+        if (books[isbn].author === author) {
+            booksByAuthor.push({isbn, ...books[isbn]})
+        }
+    }
+
+    res.send(booksByAuthor);
 });
 
 // Get all books based on title
