a817630b5ea7ff1a11bd20bcca126683023680b7
Brendan <brendanalm3@gmail.com>
2025-06-24 14:54:44 -0400

    task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -46,7 +46,30 @@ regd_users.post("/login", (req, res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    // return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+
+    if (!username) {
+        return res.status(401).json({ message: "You must be logged in to post a review." });
+    }
+
+    if (!review) {
+        return res.status(400).json({ message: "Review query is required." });
+    }
+
+    const book = books[isbn];
+    if (!book) {
+        return res.status(404).json({ message: "Book not found." });
+    }
+
+    book.reviews[username] = review;
+    return res.status(200).json({
+        message: "Review successfully added/updated.",
+        reviews: book.reviews
+    });
+
 });
 
 module.exports.authenticated = regd_users;
