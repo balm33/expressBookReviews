d3abaa163c50e47d3f8f1eee820ca72310e71104
Brendan <brendanalm3@gmail.com>
2025-06-24 14:02:36 -0400

    Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,7 +45,18 @@ public_users.get('/author/:author', function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    // return res.status(300).json({ message: "Yet to be implemented" });
+    const title = req.params.title;
+    let booksByTitle = [];
+
+    for (let isbn in books) {
+        if (books[isbn].title === title) {
+            booksByTitle.push({isbn, ...books[isbn]})
+        }
+    }
+
+    res.send(booksByTitle);
+
 });
 
 //  Get book review
